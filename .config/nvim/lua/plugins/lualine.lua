9aaa87a94f5a2d126db0ee604e5bf83c5f5ab8b7
Karan Parashar <karan@pixelos.net>
2024-08-19 16:07:40 +0530

    nvim: More minimal changes

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,16 +1,16 @@
 return {
   {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "VeryLazy",
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = "auto",
-        disabled_filetypes = { "NvimTree" },
-        ignore_focus = { "NvimTree" },
-        globalstatus = true,
+      'nvim-lualine/lualine.nvim',
+      dependencies = {'nvim-tree/nvim-web-devicons'},
+      event = "VeryLazy",
+      opts = {
+          options = {
+              icons_enabled = true,
+              theme = 'auto',
+              disabled_filetypes = {'NvimTree'},
+              ignore_focus = {'NvimTree'},
+              globalstatus = true,
+          }
       },
-    },
   },
 }
