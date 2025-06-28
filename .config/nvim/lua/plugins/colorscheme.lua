1959e91518f55c051d75bab0d0442a6904d8938a
Karan Parashar <karan.parashar@samey.ai>
2025-06-28 16:42:35 +0530

    nvim|ghostty: Switch to onedark theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,13 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"navarasu/onedark.nvim",
 		priority = 1000,
-		opts = {
-			flavour = "mocha", -- Options: latte, frappe, macchiato, mocha
-		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme "catppuccin"
-		end,
-	},
+		config = function()
+			require('onedark').setup {
+				style = 'dark'
+			}
+			-- Enable theme
+			require('onedark').load()
+		end
+	}
 }
