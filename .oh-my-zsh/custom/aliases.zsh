5f050a1a2df453cad5b5764b7d195ee140a480f8
Karan Parashar <whyredfire@gmail.com>
2026-09-15 23:28:28 +0530

    feat(zsh): Modularize and update shell configuration

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,16 @@
+# aosp
+alias hook="curl -Lo .git/hooks/commit-msg https://review.pixelos.net/tools/hooks/commit-msg && chmod u+x .git/hooks/commit-msg"
+
+# kubernetes
+alias k=kubectl
+alias kx=kubectx
+alias ks=kubeseal -o yaml
+
+# miscellaneous
+alias c=clear
+alias ff=fastfetch
+alias zshconfig="nvim ~/.zshrc"
+
+# opencode
+alias o=opencode
+alias o2=opencode2
---
+++ .oh-my-zsh/custom/variables.zsh
@@ -0,0 +1,9 @@
+# bun
+export PATH="/Users/karan/.bun/bin:$PATH"
+
+# ghostty ssh
+export TERM=xterm-256color
+
+# kubeseal
+export SEALED_SECRETS_CONTROLLER_NAME="sealed-secrets"
+export SEALED_SECRETS_CONTROLLER_NAMESPACE="sealed-secrets"
